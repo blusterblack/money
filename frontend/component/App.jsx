64fcadabecfba4f3603ebf93087db675c078f992
Le Thanh Binh <lethanhbinh010309@gmail.com>
2020-09-17 20:46:21 +0700

    add month switch to TopAppBar

--- frontend/component/App.jsx
+++ frontend/component/App.jsx
@@ -3,7 +3,9 @@ import { hot } from 'react-hot-loader';
 
 import TopAppBar from './TopAppBar';
 
-const testTopBar = { walletName: 'Main Wallet', balance: 1000.56, currency: '$' };
+const testTopBar = {
+  walletName: 'Main Wallet', balance: 1000.56, currency: '$', date: new Date(2020, 3),
+};
 function App() {
   return (
     <div className="App">
--- frontend/component/TopAppBar.jsx
+++ frontend/component/TopAppBar.jsx
@@ -4,21 +4,29 @@ import {
 } from '@material-ui/core';
 import MenuIcon from '@material-ui/icons/Menu';
 import NotificationsIcon from '@material-ui/icons/Notifications';
+import ArrowBackIosIcon from '@material-ui/icons/ArrowBackIos';
+import ArrowForwardIosIcon from '@material-ui/icons/ArrowForwardIos';
 import PropTypes from 'prop-types';
 import { makeStyles } from '@material-ui/styles';
 
 const useStyle = makeStyles({
   typo: {
     flexGrow: 1,
+    textAlign: 'center',
   },
 });
 export default function TopAppBar(props) {
-  const { walletName, balance, currency } = props;
+  const {
+    walletName, balance, currency, date,
+  } = props;
+  const monthNames = ['January', 'February', 'March', 'April', 'May', 'June',
+    'July', 'August', 'September', 'October', 'November', 'December',
+  ];
   const classes = useStyle();
   return (
     <AppBar>
       <Toolbar>
-        <Typography className={classes.typo}>{walletName + balance + currency}</Typography>
+        <Typography className={classes.typo}>{`${walletName} : ${balance}${currency}`}</Typography>
         <IconButton>
           <NotificationsIcon />
         </IconButton>
@@ -28,6 +36,12 @@ export default function TopAppBar(props) {
         </IconButton>
 
       </Toolbar>
+      <Toolbar>
+        <IconButton><ArrowBackIosIcon /></IconButton>
+        <Typography className={classes.typo}>{`${monthNames[date.getMonth()]} ${date.getFullYear()}`}</Typography>
+        <IconButton><ArrowForwardIosIcon /></IconButton>
+      </Toolbar>
+
     </AppBar>
   );
 }
@@ -35,4 +49,5 @@ TopAppBar.propTypes = {
   walletName: PropTypes.string.isRequired,
   balance: PropTypes.number.isRequired,
   currency: PropTypes.string.isRequired,
+  date: PropTypes.instanceOf(Date).isRequired,
 };
